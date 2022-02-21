c5ea9714981f0d88b65559bcea83a1cc10b4ba9a
Dorian <dorianc.vieira@gmail.com>
2022-02-21 15:59:04 -0300

    Task class

---
+++ src/main/java/com/book/task/entity/Task.java
@@ -0,0 +1,28 @@
+package com.book.task.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Task {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column
+	@NotNull(message="{NotNull.Task.name}")
+	private String name;
+	
+	@Column
+	@NotNull(message="{NotNull.Task.description}")
+	private String description;
+
+}
